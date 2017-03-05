0fba5c68076ecc292cac81c364869b040ea101e4
Sean Morris <sean@seanpaq.virgo>
2017-03-05 13:48:43 -0500

    Error template.

---
+++ source/Theme/Austere/Error.php
@@ -0,0 +1,13 @@
+<?php
+namespace SeanMorris\PressKit\Theme\Austere;
+class Error extends \SeanMorris\Theme\View
+{
+
+}
+__halt_compiler();
+?>
+<h1>HTTP <?= $object->getCode(); ?></h1>
+<p><?= $object->getMessage(); ?></p>
+<?php if(Settings::read('devmode')): ?> 
+	<pre><?= $object->indentedTrace(); ?></pre>
+<?php endif; ?>
