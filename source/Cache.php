73575b5c13dbe6bef18d56a2db6eaa3fdb395253
Sean Morris <sean@arcade>
2017-06-05 13:02:36 -0400

    Cache model

---
+++ source/Cache.php
@@ -0,0 +1,32 @@
+<?php
+namespace SeanMorris\PressKit;
+class Cache extends \SeanMorris\PressKit\Model
+{
+	protected
+		$name
+		, $bucket
+		, $expiry
+		, $content;
+	protected static
+		$table = 'Cache'
+		, $byName = [
+			'where' =>  ['AND' => [
+				['name' => '?']
+			]]
+		];
+
+	protected static function beforeWrite($instance, &$skeleton)
+	{
+		$skeleton['content'] = serialize($instance->content);
+	}
+
+	protected static function afterRead($instance)
+	{
+		if($instance->expiry > 0 && $instance->expiry <= time())
+		{
+			return FALSE;
+		}
+
+		$instance->content = unserialize($instance->content);
+	}
+}
